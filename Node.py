bcde9a83b160bbf2c9062cf03068e9b14f863024
Fisher M-W <fishermunsonwarnken@gmail.com>
2026-08-06 17:03:04 -0700

    added node storage operations

--- Node.py
+++ Node.py
@@ -1,5 +1,5 @@
 """
-Single node in a cluster
+Single storge node in a cluster
 
 One server in a Nutanix cluster. It has local storage, (in this case a plain dict) and can be healthy or down. 
 In a real cluster the Controler Virtual Machine woudl serve I/O for the node's disks; here the Node plays both 
@@ -15,3 +15,27 @@ class Node:
         self.alive = True
         
 #------Storage Operations-------
+
+    """ store one of the RF copies that exists across the cluster """
+    def write(self, key:str, value:str) -> None:
+        self._data[key] = value
+    """ drop this nodes replica of the key (we need this when rebalancing) """
+    def delete(self, key:str) -> None:
+        self._data.pop(key, None)
+    
+    def holds(self, key:str) -> bool:
+        return key in self._data
+    
+    """ all keys this node currently stores """
+    def keys(self) -> set[str]:
+        return set(self._data.keys())
+
+    def read(self, key:str) -> str:
+        return self._data.get(key)
+
+
+    # ----- add functions for failure simulation -----
+    # need kill, revive, and repair 
+    """ simulate node failure, data does not clear, simply unreachable """
+    def kill(self) -> None:
+        self.alive = False
